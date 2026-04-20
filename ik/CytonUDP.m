aaaea2141cb52986b52ac639cbf0a63f019cd482
Philip Deaton <philipdeaton826@gmail.com>
2026-04-20 00:28:33 -0400

    actin udp class

---
+++ ik/CytonUDP.m
@@ -0,0 +1,43 @@
+classdef CytonUDP < handle
+
+    properties (Access = private)
+        udpActin
+    end
+
+    methods
+        function this = CytonUDP
+            this.initCytonUDP();
+        end
+
+        function delete(this)
+            if ~isempty(this.udpActin)
+                this.udpActin.close();
+                disp('[CytonUDP] Actin UDP closed.');
+            end
+        end
+
+        function moveActinCyton(this, q, g)
+            % Send joint angles to Actin Viewer over UDP
+            % g: value for gripper
+            % q: 7x1 joint angles in Actin space (radians)
+            % Actin expects 8 float64 values: 7 joint angles + gripper aperture
+
+            if numel(q) ~= 7
+                error('CytonUDP:invalidInput', 'q must have 7 elements, got %d.', numel(q));
+            end
+
+            desiredAngles = [q(:); g];
+            this.udpActin.putData(typecast(double(desiredAngles), 'uint8'));
+        end
+
+    end
+
+    methods (Access = private)
+        function initCytonUDP(this)
+            this.udpActin = PnetClass(8889, 8888, '127.0.0.1');
+            this.udpActin.initialize();
+            disp('[init_cyton_udp] Actin UDP initialized');
+        end
+    end
+
+end
--- ik/IkineWrapper.m
+++ ik/IkineWrapper.m
@@ -1,22 +1,40 @@
 % ikine Wrapper
 
-function IkineWrapper()
+function IkineWrapper(isActin)
+
+if nargin < 1
+    isActin = false;
+end
+
+if isActin
+    currDir = cd;
+    cd('C:\GitHub\MiniVIE');
+    MiniVIE.configurePath();
+    cd(currDir);
+    udp = CytonUDP();
+end
 
 q_initial_actin = zeros(7, 1);  % Initialize joint angles for the actuator
 z_fixed = 0.5;                   % Fixed z-coordinate for the IK calculation
 roll_fixed = 0;
 pitch_fixed = 0;
 yaw_fixed = 0;
+gripper_fixed = 0.01;
 dt = 0.1;                        % Time step for the loop
 
 lm  = HandLandMarker();
 ik  = CytonIK(q_initial_actin);
 
+
 while true
     xy = lm.getXY();           % blocks until data arrives
+    % get z from force sensor
+    % get rotation from myoband
     ik = ik.updateIK([xy(1); xy(2); z_fixed; roll_fixed; pitch_fixed; yaw_fixed]);
     disp(ik.qActin);
-    % moveActinCyton([ik.qActin; 0.01]);
+    if isActin
+        udp.moveActinCyton(ik.qActin, gripper_fixed);
+    end
     pause(dt);
 end
 
