634b27cc7153c7ec97a523aa27fa61a4bba4a7b2
Michael DeLeo <michaeldeleo31@yahoo.com>
2026-04-22 21:44:23 -0400

    update

--- gripper_rotation/orientation.m
+++ gripper_rotation/orientation.m
@@ -1,5 +1,4 @@
-%function q_7 = orientation(hMyo, q, neutral_gyro, dt)
-function q_7 = orientation(hMyo, roll_in, neutral_gyro, dt)
+function q_7 = orientation(hMyo, q, neutral_gyro, dt)
     gyroDeg = hMyo.Gyroscope(:)';
     kB = 1.5;
     
@@ -14,10 +13,8 @@ function q_7 = orientation(hMyo, roll_in, neutral_gyro, dt)
     g_rad = deg2rad(g);
 
     % Calculate new position
-    %raw_q7 = q(7) + g_rad * dt; 
-    raw_roll = roll_in + g_rad * dt; 
+    raw_q7 = q(7) + g_rad * dt; 
     
-    %q_7 = atan2(sin(raw_q7), cos(raw_q7));
-    q_7 = atan2(sin(raw_roll), cos(raw_roll));
+    q_7 = atan2(sin(raw_q7), cos(raw_q7));
 
 end
